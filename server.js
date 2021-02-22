1a133a0be8f70afa654579d31ad25565da1e75a7
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-02-22 20:32:36 +0100

    Fix typo

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/history', (req, res, next) => {
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
-})
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
