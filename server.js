4a2de260f39c4fd8526bf73ba6f1d9cff0a27ded
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-02-22 19:52:09 +0100

    Add middleware (express.static) for the external resources

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', (req, res) => {
   res.show('index.html');
 });
@@ -28,14 +30,6 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.show('history.html');
-});
-
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '/style.css'));
-});
-
-app.get('/test.png', (req, res) => {
-  res.sendFile(path.join(__dirname, '/test.png'));
 });
 
 app.use((req, res) => {
