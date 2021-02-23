7c63320ec51e0cb0f062139cde8b0e28ddc36043
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-02-23 21:35:40 +0100

    Add sending image to the form with validation - not working

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', '.hbs');
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -27,10 +27,10 @@ app.get('/contact', (req, res) => {
 
 app.post('/contact/send-message', (req, res) => {
 
-  const { author, sender, title, message } = req.body;
+  const { author, sender, title, image, message } = req.body;
 
-  if (author && sender && title && message) {
-    res.render('contact', { isSent: true });
+  if (author && sender && title && image && message) {
+    res.render('contact', { isSent: true, filename: req.body.image });
   }
   else {
     res.render('contact', { isError: true });
